ebe77f34d6f657aedb370485f4955f888fe52b3d
Mikhail Cherviakov <m.cherviakov@fxpro.com>
2012-07-03 11:41:20 +0300

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,65 +1,102 @@
--- $Id$
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
 
--- i18n
-create sequence language_id;
-create table "language" (
-	"id"		smallint not null PRIMARY KEY default nextval('language_id'),
-	"name"		varchar(16) not null,
-	"native"	varchar(16) null,
-	"code"		varchar(2) not null
+
+CREATE SEQUENCE user_id
+    START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE TABLE "user" (
+    id			bigint PRIMARY KEY DEFAULT nextval('user_id'::regclass) NOT NULL,
+    name		character varying(64) NOT NULL,
+    surname		character varying(64) NOT NULL,
+    email		character varying(128) NOT NULL,
+    username	character varying(64) NOT NULL,
+    password	character varying(32) NOT NULL
 );
 
-create sequence name_id;
-create table "name" (
-	"id"			bigint not null PRIMARY KEY default nextval('name_id'),
-	"value"			varchar(64) not null,
-	language_id		bigint not null references "language"(id) on update cascade on delete restrict,
-	bean_name		varchar(32) not null,
-	bean_attr		varchar(32) not null,
-	bean_id			bigint not null
-);
-create index name_language_idx on "name"(language_id);
-create index name_bean_name_id_idx on "name"(bean_name, bean_id);
-create unique index name_language_bean_name_id_uidx on "name"(language_id, bean_name, bean_id);
 
-
--- common
-
-create sequence user_id;
-create table "user" (
-	"id"		bigint not null PRIMARY KEY default nextval('user_id'),
-	"name"		varchar(64) not null,
-	"surname"	varchar(64) not null,
-	"email"		varchar(128) not null,
-	"username" 	varchar(64) not null,
-	"password"	varchar(32) not null
+CREATE SEQUENCE offer_type_id
+    START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE TABLE offer_type (
+    id		bigint PRIMARY KEY DEFAULT nextval('offer_type_id'::regclass) NOT NULL,
+    name	character varying(32) NOT NULL
 );
 
-create sequence offer_type_id;
-create table "offer_type" (
-	"id"		bigint not null PRIMARY KEY default nextval('offer_type_id'),
-	"name_id"	bigint not null references "language"(id) on update cascade on delete restrict
-);
-create index type_parent_idx on type(parent_id);
 
 
-create sequence offer_id;
-create table "offer" (
-	"id"			bigint not null PRIMARY KEY default nextval('offer_id'),
-	"name"			varchar(64) not null,
-	"description"	varchar(512) null,
-	"user_id"		bigint not null references "user"(id) on update cascade on delete cascade
+CREATE SEQUENCE property_type_id
+    START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE TABLE property_type (
+    id		bigint PRIMARY KEY DEFAULT nextval('property_type_id'::regclass) NOT NULL
+    name	character varying(32) NOT NULL
 );
-create index sale_user_idx on sale(user_id);
 
-create sequence item_id;
-create table item (
-	"id"			bigint not null PRIMARY KEY default nextval('item_id'),
-	"name"			varchar(64) not null,
-	"description"	varchar(512) null,
-	"type_id"		bigint null references "type"(id) on update cascade on delete cascade,
-	"sale_id"		bigint not null references "sale"(id) on update cascade on delete cascade,
-	"price"			numeric(20,2) null
+
+CREATE SEQUENCE property_id
+    START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE TABLE property (
+    id			bigint PRIMARY KEY DEFAULT nextval('property_id'::regclass) NOT NULL,
+    name		character varying(128) NOT NULL,
+    description character varying(512),
+    price		numeric(10,2) NULL,
+    user_id				bigint NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    property_type_id	bigint NOT NULL REFERENCES property_type(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    offer_type_id		bigint NOT NULL REFERENCES offer_type(id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
-create index item_sale_idx on item(sale_id);
-create index item_type_idx on item(type_id);
+CREATE INDEX property_user_id_idx ON property USING btree (user_id);
+CREATE INDEX property_type_id_idx ON property USING btree (property_type_id);
+CREATE INDEX property_offer_id_idx ON property USING btree (offer_type_id);
+
+
+CREATE SEQUENCE unit_id
+    START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE TABLE unit (
+    id		bigint PRIMARY KEY DEFAULT nextval('unit_id'::regclass) NOT NULL,
+    name	character varying(32) NOT NULL
+);
+
+
+CREATE SEQUENCE feature_type_id
+    START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE TABLE feature_type (
+    id		bigint PRIMARY KEY DEFAULT nextval('feature_type_id'::regclass) NOT NULL,
+    name	character varying(32) NOT NULL,
+    unit_id bigint NULL REFERENCES unit(id) ON UPDATE CASCADE ON DELETE SET NULL
+);
+CREATE INDEX feature_type_unit_id_idx ON feature_type USING btree (unit_id);
+
+
+CREATE SEQUENCE feature_id
+    START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
+CREATE TABLE feature (
+    id bigint	PRIMARY KEY DEFAULT nextval('feature_id'::regclass) NOT NULL,
+    content		character varying(128),
+    value		numeric(20,2),
+    type_id		bigint NOT NULL REFERENCES feature_type(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    property_id bigint NOT NULL REFERENCES property(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+CREATE INDEX feature_property_id_idx ON feature USING btree (property_id);
+CREATE INDEX feature_type_id_idx ON feature USING btree (type_id);
+
+
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+
+
+-- CREATE SEQUENCE language_id
+--     START WITH 1
+--     INCREMENT BY 1
+--     NO MINVALUE
+--     NO MAXVALUE
+--     CACHE 1;
+--
+-- CREATE TABLE language (
+--     id bigint DEFAULT nextval('feature_type_id'::regclass) NOT NULL,
+--     name character varying(32) NOT NULL,
+-- );
