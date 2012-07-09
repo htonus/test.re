d2e0d42f96a74c4793882feedd09ad989b436f5d
Mikhail Cherviakov <htonus@gmail.com>
2012-07-09 22:09:27 +0300

    initial data

---
+++ db/data.sql
@@ -0,0 +1,10 @@
+insert into property_type (id, name) values (1, 'room');
+insert into property_type (id, name) values (2, 'flat');
+insert into property_type (id, name) values (3, 'appartment');
+insert into property_type (id, name) values (4, 'house');
+
+insert into offer_type (id, name) values (1, 'buy');
+insert into offer_type (id, name) values (2, 'sell');
+insert into offer_type (id, name) values (3, 'rent');
+insert into offer_type (id, name) values (4, 'lease');
+
