81766a1b3b6522a823cd9989396edc0a0a26dbe6
Mikhail Cherviakov <htonus@gmail.com>
2012-07-29 01:38:06 +0300

    config

--- config.inc.php
+++ config.inc.php
@@ -17,11 +17,11 @@
 	define('PATH_SOURCE', PATH_BASE.'src'.DIRECTORY_SEPARATOR.PATH_SOURCE_DIR);
 	define('PATH_WEB', 'http://www.'.DOMAIN.'/');
 	define('PATH_WEB_ADMIN', 'http://admin.'.DOMAIN.'/');
-	define('PATH_WEB_PIX', '/pix/');
-	define('PATH_WEB_IMG', '/img/');
-	define('PATH_WEB_JS', '/js/');
-	define('PATH_WEB_CSS', '/css/');
-
+	define('PATH_WEB_PIX', '/pix/'); // dynamic stuff
+	define('PATH_WEB_IMG', '/i/'); // static stuff
+	define('PATH_WEB_JS', '/js/'); // static stuff
+	define('PATH_WEB_CSS', '/css/'); // static stuff
+	
 	// shared classes
 	define(
 		'PATH_CLASSES',
