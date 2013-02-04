a4549c3ad64f6fe36738b0dcb68174b48de91864
Mike <htonus@gmail.com>
2013-02-04 04:24:16 -0800

    Create schema2pgsql.php

---
+++ src/user/webroot/schema2pgsql.php
@@ -0,0 +1,19 @@
+<?php
+    $appPath =
+  realpath(
+		dirname(__FILE__).DIRECTORY_SEPARATOR
+		.'..'.DIRECTORY_SEPARATOR
+		.'..'.DIRECTORY_SEPARATOR
+		.'..'.DIRECTORY_SEPARATOR
+	)
+	.DIRECTORY_SEPARATOR;
+
+if (file_exists($appPath.'config.inc.php'))
+	require_once ($appPath.'config.inc.php');
+else
+	require_once ($appPath.'config.inc.php.tpl');
+
+include '../../classes/Auto/schema.php';
+
+echo $schema->toDialectString(PostgresDialect::me());
+?>
